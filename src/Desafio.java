9ac560f6be300ea0428ab03754a58c618948f860
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-11-17 11:01:36 -0300

    iniciando desafio

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
 
 public class Desafio {
+
+
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
+        Scanner opcao2 = new Scanner(System.in);
+        int opcao = 0;
+
+
         System.out.print("Digite seu nome:  ");
         String nome = leitura.nextLine();
 
@@ -14,43 +20,49 @@ public class Desafio {
 
         System.out.println("********************************** ");
 
-        System.out.println( "\n Nome do cliente: " + nome);
-        System.out.println( "\n Tipo de conta " + conta);
-        System.out.println( "\n saldo: " + valor +"R$");
+        System.out.println("\n Nome do cliente: " + nome);
+        System.out.println("\n Tipo de conta " + conta);
+        System.out.println("\n saldo: " + valor + "R$");
         System.out.println("\n ********************************** ");
 
+        String menu = """
+                    ** Digite a opção **
+                    1 - Consultar Saldo
+                    2 - Tranferiar valor
+                    3 - rebeber valor
+                    4 - sair
+                """;
+        System.out.print(" ");
 
 
-/*
-        System.out.print("Digite 1 para saldo  ");
-        int saldo = leitura.nextInt();
 
-        System.out.print("Digite 2 para receber  ");
-        int receber = leitura.nextInt();
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = opcao2.nextInt();
 
-        System.out.print("Digite 3 para tranferir  ");
-        int transferir = leitura.nextInt();
+            if (opcao == 1) {
+                System.out.println("\nSaldo: " + valor +"R$");
+            } else if (opcao == 2) {
+                System.out.print("\nQual o valor que deseja transferir: ");
+                double valorConta = opcao2.nextDouble();
+                if (valorConta > valor) {
+                    System.out.print("\nNão tem saldo suficiente \n");
+                } else {
+                    valor -= valorConta;
+                    System.out.print("Tranferencia realizado con sucesso, Novo Saldo: " + valor +"R$ \n");
 
-        System.out.print("Digite 4 para sair  ");
-        int sair = leitura.nextInt();
+                }
+            } else if (opcao == 3) {
+                System.out.print("Valor Recebido: ");
+                double valorRecebido = opcao2.nextDouble();
+                valor += valorRecebido;
+        } else if (opcao != 4) {
+                System.out.print("opção invalida ");
 
-        if (saldo == 1) {
-            System.out.println("Valor em conta: " + saldo +"R$");
+            }
+
+
         }
 
 
-
-        System.out.print("********************************** ");
-        System.out.print("Dados do Cliente");
-        */
-
-    }
-}
-
-/*
-
-
-
-
-
- */
+}}
