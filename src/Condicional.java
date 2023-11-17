5386d9d32890b543dd8a3e4df1bee0abf738314a
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-11-16 22:30:28 -0300

    Iniciando curso java

---
+++ src/Condicional.java
@@ -0,0 +1,17 @@
+public class Condicional {
+    public static void main(String[] args) {
+        System.out.println(" robervan esta aqui!!!");
+        System.out.println("filme: Era do gelo");
+
+        int ano = 2023;
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 8.1;
+
+        if (ano >= 2022) {
+            System.out.println("lançamento ok");
+        }
+
+
+
+    }
+}
