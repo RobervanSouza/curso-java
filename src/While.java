2d7af74e2e923e03ba8b9be1533856f0cc751724
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-11-17 10:01:21 -0300

    criando while

--- src/While.java
+++ src/While.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class While {
     public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double media = 0;
+        double nota = 0;
+        int totalAvaliacao = 0;
 
+        while (nota != -1) {
+
+            System.out.println("Digita a avaliação do filme ou -1 para encerrar ");
+            nota = leitura.nextDouble();
+            if (nota != -1){
+
+            media = media + nota; // duas formas de usar media += nota;
+            totalAvaliacao++;
+            }
+
+        }
+        System.out.println("Media de avaliação geral foi: " + media/totalAvaliacao );
     }
 }
